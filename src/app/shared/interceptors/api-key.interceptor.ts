16d8a11325af9121fac23f7471502f7cd787eb08
Svetoslav Busarov <svetoslav.busarov@gmail.com>
2025-02-02 17:43:17 +0200

    test interceptor

--- src/app/shared/interceptors/api-key.interceptor.ts
+++ src/app/shared/interceptors/api-key.interceptor.ts
@@ -1,11 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
 export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
-  const apiKey = environment.apiKey;
-  const modifiedReq = req.clone({
-    setHeaders: {
-      "X-API-KEY": apiKey
-    }
-  });
-  return next(modifiedReq);
+  // const apiKey = environment.apiKey;
+  // const modifiedReq = req.clone({
+  //   setHeaders: {
+  //     "X-API-KEY": apiKey
+  //   }
+  // });
+  // return next(modifiedReq);
+  console.log(req.url);
+  console.log(req.headers);
+  console.log(environment.apiKey);
+  return next(req);
 };
