59da5fbc71084b02435143ca49c25a1e2167a5f8
Svetoslav Busarov <svetoslav.busarov@gmail.com>
2025-02-02 17:49:50 +0200

    add req header to request

--- src/app/shared/interceptors/api-key.interceptor.ts
+++ src/app/shared/interceptors/api-key.interceptor.ts
@@ -1,15 +1,13 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
 export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
-  // const apiKey = environment.apiKey;
-  // const modifiedReq = req.clone({
-  //   setHeaders: {
-  //     "X-API-KEY": apiKey
-  //   }
-  // });
-  // return next(modifiedReq);
-  console.log(req.url);
-  console.log(req.headers);
+  const apiKey = environment.apiKey;
+  const modifiedReq = req.clone({
+    setHeaders: {
+      "X-API-KEY": apiKey
+    }
+  });
+  console.log(modifiedReq.headers);
   console.log(environment.apiKey);
-  return next(req);
+  return next(modifiedReq);
 };
