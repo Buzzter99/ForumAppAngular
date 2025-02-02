7c3dbbae9965fd54204fa2eb078078258e7fdff8
Svetoslav Busarov <svetoslav.busarov@gmail.com>
2025-02-02 16:33:36 +0200

    rollback changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { apiKeyInterceptor } from './shared/interceptors/api-key.interceptor';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withInterceptors([apiKeyInterceptor])),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+  providers: [provideHttpClient(),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
 };
